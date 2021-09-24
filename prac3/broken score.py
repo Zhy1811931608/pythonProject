8d17483e2aa35246ac9eb8cecd22a5f3d3d85669
sjc <1811931608@qq.com>
2021-09-24 15:28:13 +0800

    add the function about print

--- prac3/broken score.py
+++ prac3/broken score.py
@@ -1,12 +1,14 @@
-
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:                #相当于 if score<100 and score>=90
-    print("Excellent")
-elif score >= 50:
-    print("Possibal")
-else:
-     print("Bad")
-
+def main():
+    score = float(input("Enter score: "))
+    print(get_score(score))
+def get_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:  # 相当于 if score<100 and score>=90
+        return "Excellent"
+    elif score >= 50:
+        return "Possibal"
+    else:
+        return "Bad"
+main()
 
