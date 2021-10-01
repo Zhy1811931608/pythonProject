6f24cd7317b8d12c30e7f3a8e71c02297a623fc0
sjc <1811931608@qq.com>
2021-10-01 20:33:44 +0800

    write a program  output various things about list

---
+++ prac4/lists exercises.py
@@ -0,0 +1,16 @@
+numbers = []
+sum=0
+for j in range(5):
+    Number=int(input('Number: '))
+    numbers.append(Number)
+largest_number = max(numbers)
+smallest_number = min(numbers)
+length = len(numbers)
+for number in numbers:
+    sum=sum+number
+average = sum/length
+print('The first number is {}'.format(numbers[0]))
+print('The last number is {}'.format(numbers[4]))
+print('The smallest number is {}'.format(smallest_number))
+print('The largest number is {}'.format(largest_number))
+print('The average of the numbers is {}'.format(average))
