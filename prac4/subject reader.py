bbaea61d744937df1306dbd24cbcdecf4948a367
sjc <1811931608@qq.com>
2021-10-01 17:01:02 +0800

    Add a new function to display subject details

--- prac4/subject reader.py
+++ prac4/subject reader.py
@@ -9,6 +9,7 @@ FILENAME = "subject data.txt"
 def main():
     data =get_data()
     print(data)
+    print_detail()
 
 
 def get_data():
@@ -16,11 +17,23 @@ def get_data():
     input_file = open(FILENAME)
     parts = []
     for line in input_file:
-        part = line.split(',')
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        part = line.split(',')  # Separate the data into its parts
         part[2] = int(part[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(part)  # See if that worked
+        print("----------")
         parts.append(part)
     input_file.close()
     return parts
 
+def print_detail():
+    input_file = open(FILENAME)
+    for line in input_file:
+        line = line.strip()
+        part = line.split(',')
+        print('{} is taught by {} and has {} students'.format(part[0], part[1], part[2]))
+    input_file.close()
 
 main()
