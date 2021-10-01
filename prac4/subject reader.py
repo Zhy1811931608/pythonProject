a0ab7e09f4e622d9b369c7264a84226e5c2f1be1
sjc <1811931608@qq.com>
2021-10-01 16:50:09 +0800

    put the data in a list and return

--- prac4/subject reader.py
+++ prac4/subject reader.py
@@ -7,23 +7,20 @@ FILENAME = "subject data.txt"
 
 
 def main():
-    data = get_data()
+    data =get_data()
     print(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    parts = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        part = line.split(',')
+        part[2] = int(part[2])  # Make the number an integer (ignore PyCharm's warning)
+        parts.append(part)
     input_file.close()
+    return parts
 
 
 main()
