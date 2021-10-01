b64e8e141d528a5f9a0dc31cd10d1d0c8e0b0ddd
sjc <1811931608@qq.com>
2021-10-01 22:44:01 +0800

    answer the question

---
+++ prac4/lists warmup.py
@@ -0,0 +1,10 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers[3:4],numbers[:-1],numbers+[5,6,3])
+numbers[0] = 'ten'
+numbers[-1] = 1
+another_numbers=numbers[2:6]
+if 9 in numbers:
+    print('yes')
+print(numbers)
+print(another_numbers)
+
